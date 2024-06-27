fe935ab535eafb5893b9ab06bc2ba055e4776bba
peyush.pushkar <peyush.pushkar@mail.schwarz>
2024-06-27 09:51:29 +0200

    add the new InMemory

--- DemoProject01/Program.cs
+++ DemoProject01/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DemoProjectDbContext>(options=> options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<DemoProjectDbContext>(options=> options.UseInMemoryDatabase("DefaultDb"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
